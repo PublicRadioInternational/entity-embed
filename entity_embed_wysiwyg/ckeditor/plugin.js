2ca377e00ce8d65334b37bd44ed954a2863a9971
Patrick Coffey <patrickcoffey48@gmail.com>
2014-11-21 13:00:09 -0600

    Initial commit with entity_embed module, and in-progress entity_embed_wysiwyg module

---
+++ entity_embed_wysiwyg/ckeditor/plugin.js
@@ -0,0 +1,49 @@
+/**
+ * @file
+ * JavaScript that defines the entity_embed_wysiwyg_add_token
+ * button and handles events.
+ */
+'use strict';
+(function($) {
+  CKEDITOR.plugins.add('entity_embed_wysiwyg_plugin', {
+    requires: ['iframedialog'],
+    init: function(editor) {
+
+      // Create Dialog that allows users to select entities that should be embedded.
+      CKEDITOR.dialog.add('select_entity', function (editor) {
+        return {
+          title: Drupal.t('Embed an Entity'),
+          minWidth: 700,
+          minHeight: 400,
+          contents: [{
+            id: 'entity-embed-select',
+            label: Drupal.t('Embed an Entity'),
+            expand: true,
+            elements: [{
+              type: 'iframe',
+              src: Drupal.settings.basePath + 'admin/config/content/wysiwyg/entity_embed/select',
+              width: '100%',
+              height: '400px',
+              onContentLoad: function() {
+
+              },
+            }]
+          }],
+          onOk: function() {
+
+          }
+        };
+      });
+
+      // Create our command for launching the node edit form.
+      editor.addCommand('launch_entity_embed_select', new CKEDITOR.dialogCommand('select_entity'));
+
+      // Create our button.
+      editor.ui.addButton('entity_embed_wysiwyg_add_token', {
+        label: Drupal.t('Entity Embed Select View'),
+        command: 'launch_entity_embed_select',
+        icon: this.path + 'add_embed_token.png'
+      });
+    }
+  });
+})(jQuery);
